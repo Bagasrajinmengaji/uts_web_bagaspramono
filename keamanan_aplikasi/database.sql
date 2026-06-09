987fc6f97f379b0b9332afe751a5a018d179caaa
Bagasrajinmengaji <pramonobagas01@gmail.com>
2026-06-10 00:38:59 +0700

    test

--- keamanan_aplikasi/database.sql
+++ keamanan_aplikasi/database.sql
@@ -22,3 +22,4 @@ CREATE TABLE IF NOT EXISTS `transaksi` (
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- test
